0f6c60ddc8f1906db20f72aef2e753ff2721735f
NoirMakkuro <eigo.kenshiro.cap@gmail.com>
2026-06-07 14:30:48 -0700

    feat: implement GitHub auth with Better Auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import prisma from "@/lib/prisma";
+
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    },
+  },
+  plugins: [nextCookies()],
+});
