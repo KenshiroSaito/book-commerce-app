1a3ccbf56797ac57dc43baa145e7b120116b6acf
NoirMakkuro <eigo.kenshiro.cap@gmail.com>
2026-06-07 14:31:03 -0700

    feat: add header, book components, and layout

--- src/app/components/Book.tsx
+++ src/app/components/Book.tsx
@@ -43,12 +43,12 @@ const Book = ({ book }: any) => {
 
         {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
           <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">本を購入しますか？</h3>
+            <h3 className="text-xl mb-4">Would you like to buy a book?</h3>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              購入する
+              Purchase
             </button>
             <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              キャンセル
+              Cancel
             </button>
           </div>
         </div> */}
