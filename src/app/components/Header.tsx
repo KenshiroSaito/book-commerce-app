6a999a6198fd2d81af14b45c423a280e0b9955e8
NoirMakkuro <eigo.kenshiro.cap@gmail.com>
2026-06-07 18:25:04 -0700

    feat: add logout and configure image domains

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
   const { data: session } = authClient.useSession();
   const user = session?.user;
-  console.log(user);
+  const router = useRouter();
 
   return (
     <header className="bg-slate-600 text-gray-100 shadow-lg">
@@ -23,11 +24,25 @@ const Header = () => {
             Home
           </Link>
           <Link
-            href="/login"
+            href={user ? "/profile" : "/login"}
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
-            Log In
+            {user ? "Profile" : "Log In"}
           </Link>
+
+          {user ? (
+            <button
+              onClick={async () => {
+                await authClient.signOut();
+                router.push("/login");
+              }}
+              className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              LogOut
+            </button>
+          ) : (
+            ""
+          )}
 
           <Link href={`/profile`}>
             <Image
