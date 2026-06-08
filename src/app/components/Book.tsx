16f26715902de06a3324101d55fa8c6333ccccca
NoirMakkuro <eigo.kenshiro.cap@gmail.com>
2026-06-07 18:24:48 -0700

    feat: display books and add purchase modal flow

--- src/app/components/Book.tsx
+++ src/app/components/Book.tsx
@@ -2,9 +2,38 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { BookType } from "../types/types";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
-// eslint-disable-next-line react/display-name
-const Book = ({ book }: any) => {
+type BookProps = {
+  book: BookType;
+};
+
+const Book = ({ book }: BookProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const {data: session} = authClient.useSession();
+  const user = session?.user;
+  const router = useRouter();
+  
+  const handlePurchaseClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCancel = () => {
+    setShowModal(false);
+  };
+
+  const handlePurchaseConfirm = () => {
+    if(!user) {
+      setShowModal(false);
+      router.push("/login")
+    } else {
+      // Stripe
+    }
+  }
+
   return (
     <>
       {/* Animation title */}
@@ -25,10 +54,13 @@ const Book = ({ book }: any) => {
       `}</style>
 
       <div className="flex flex-col items-center m-4">
-        <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+        <a
+          onClick={handlePurchaseClick}
+          className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none"
+        >
           <Image
             priority
-            src={book.thumbnail}
+            src={book.thumbnail.url}
             alt={book.title}
             width={450}
             height={350}
@@ -40,18 +72,25 @@ const Book = ({ book }: any) => {
             <p className="mt-2 text-md text-slate-700">Price: ${book.price}</p>
           </div>
         </a>
-
-        {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-          <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">Would you like to buy a book?</h3>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              Purchase
-            </button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              Cancel
-            </button>
+        {showModal && (
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900/50 flex justify-center items-center modal">
+            <div className="bg-white p-8 rounded-lg">
+              <h3 className="text-xl mb-4">Would you like to buy a book?</h3>
+              <button
+                onClick={handlePurchaseConfirm}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+              >
+                Purchase
+              </button>
+              <button
+                onClick={handleCancel}
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Cancel
+              </button>
+            </div>
           </div>
-        </div> */}
+        )}
       </div>
     </>
   );
