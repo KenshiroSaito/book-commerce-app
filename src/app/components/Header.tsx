7af4f788dcf2ade577a2ac9ea0a851744de2430a
NoirMakkuro <eigo.kenshiro.cap@gmail.com>
2026-06-08 21:46:11 -0700

    chore: update header display and next config

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
                 await authClient.signOut();
                 router.push("/login");
               }}
-              className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
             >
               LogOut
             </button>
